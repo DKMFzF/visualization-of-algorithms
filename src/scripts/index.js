b837145027df61b0f5325bb9a3d6d1870a9753a2
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-11-21 14:13:45 +0700

    feat(index.js): add scroll page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
+import fullpage from 'fullpage-js-geek'; 
 import '../pages/index.css';
+
+// scroll page
+document.addEventListener("DOMContentLoaded", function () {
+    new fullpage("#fullpage", {
+      scrollingSpeed: 1000,
+      autoScrolling: true,
+      navigation: false,
+    });
+  });
