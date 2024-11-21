b4d7d5878cf6674e2c107b6e657ff687d57a97eb
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2024-11-21 13:01:40 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.html'],
+    theme: {
+      extend: {
+        fontFamily: {
+          'sans': ['"Syne"', 'sans-serif']
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
